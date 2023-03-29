b34b73108fa6ff77d85469acb681f2c7a173b3dc
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-03-29 12:41:38 +0900

    [Gold III] Title: 사다리 조작, Time: 7264 ms, Memory: 116820 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -0,0 +1,48 @@
+n, m, h = map(int, input().split())
+
+graph = [[0]*n for _ in range(h)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a-1][b-1] = 1
+    graph[a-1][b] = -1
+
+answer = 4
+
+
+def check():
+    for i in range(n):
+        y = i
+        for x in range(h):
+            if graph[x][y] == 1:
+                y += 1
+            elif graph[x][y] == -1:
+                y -= 1
+
+        if y != i:
+            return False
+    return True
+
+
+def dfs(depth, last):
+    global answer
+
+    if depth >= answer:
+        return
+
+    if check() and depth < answer:
+        answer = min(answer, depth)
+        return
+    else:
+        for x in range(h):
+            for y in range(n-1):
+                if x*n + y > last:
+                    if graph[x][y] == 0 and graph[x][y+1] == 0:
+                        graph[x][y] = 1
+                        graph[x][y+1] = -1
+                        dfs(depth+1, x*n + y)
+                        graph[x][y] = 0
+                        graph[x][y+1] = 0
+
+
+dfs(0, -1)
+print(-1 if answer > 3 else answer)
