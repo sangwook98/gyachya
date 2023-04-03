f25c81846aff558fbeab3e4601032a13c9380cf3
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-04-03 11:24:18 +0900

    [Gold III] Title: 나무 재테크, Time: 260 ms, Memory: 117168 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,12 +1,12 @@
 n, m, k = map(int, input().split())
 graph = [[5]*n for _ in range(n)]
 poop = [list(map(int, input().split())) for _ in range(n)]
-trees = [[[] for _ in range(n)] for _ in range(n)]
-deads = [[[] for _ in range(n)] for _ in range(n)]
+trees = [[{} for _ in range(n)] for _ in range(n)]
+
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    trees[a-1][b-1].append(c)
+    trees[a-1][b-1][c] = 1
 
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
@@ -19,33 +19,45 @@ def is_valid(nx, ny):
 def spring_summer():
     for i in range(n):
         for j in range(n):
-            tree = trees[i][j]
-            index = len(tree)
-            temp = 0
-            for k in range(len(tree)-1, -1, -1):
-                if tree[k] <= graph[i][j]:
-                    graph[i][j] -= tree[k]
-                    tree[k] += 1
-                    index = k
-                else:
-                    temp += tree[k]//2
+            if trees[i][j]:
+                die = 0
+                survived = 0
+                temp = {}
 
-            graph[i][j] += temp
-            trees[i][j] = tree[index:]
+                for age in sorted(trees[i][j].keys()):
+                    if trees[i][j][age]*age <= graph[i][j]:
+                        graph[i][j] -= trees[i][j][age]*age
+                        temp[age+1] = trees[i][j][age]
+                    else:
+                        survived = graph[i][j] // age
+
+                        if not survived:
+                            die += (trees[i][j][age] - survived)*(age // 2)
+                            continue
+                        graph[i][j] -= survived*age
+                        die += (trees[i][j][age] - survived)*(age // 2)
+
+                        temp[age+1] = survived
+                trees[i][j] = temp
+                graph[i][j] += die
 
 
 def fall_winter():
     # fall
     for i in range(n):
         for j in range(n):
-            tree = trees[i][j]
-            for k in range(len(tree)):
-                if tree[k] % 5 == 0:
+            if trees[i][j]:
+                tmp = 0
+                for age in trees[i][j].keys():
+                    if age % 5 == 0:
+                        tmp += trees[i][j][age]
+
+                if tmp > 0:
                     for p in range(8):
                         nx = i + dx[p]
                         ny = j + dy[p]
                         if is_valid(nx, ny):
-                            trees[nx][ny].append(1)
+                            trees[nx][ny][1] = trees[nx][ny].get(1, 0) + tmp
             # winter
             graph[i][j] += poop[i][j]
 
@@ -61,11 +73,14 @@ def pp():
 
 for l in range(k):
     spring_summer()
+    # pp()
     fall_winter()
+    # pp()
     # print('{}년 끝'.format(l+1))
 
 answer = 0
 for i in range(n):
     for j in range(n):
-        answer += len(trees[i][j])
+        for value in trees[i][j].values():
+            answer += value
 print(answer)
