fa7ed2b5618a26776f296e42f31afcfca2ad5f83
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-04-08 10:29:50 +0900

    [Gold V] Title: 회문, Time: 180 ms, Memory: 116256 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -1,29 +1,20 @@
-t = int(input())
-
-
 def main(words):
     n = len(words)
     left = 0
     right = n-1
-    count = 0
     while left < right:
         if words[left] == words[right]:
             left += 1
             right -= 1
         else:
-            a = words[left:right]
-            r_a = a[::-1]
-            b = words[left+1:right+1]
-            r_b = b[::-1]
-            if a == r_a or b == r_b:
-                count = 1
-                break
+            a = words[left+1:right+1]
+            b = words[left:right]
+            if a == a[::-1] or b == b[::-1]:
+                return 1
             else:
-                count = 2
-                break
-
-    return count
+                return 2
+    return 0
 
 
-for _ in range(t):
-    print(main(input()))
+for _ in range(int(input())):
+    print(main(input().rstrip()))
