318e6add31be47782ebae3b41b19fc102046a8fb
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-04-16 12:26:53 +0900

    [Gold III] Title: 컬러볼, Time: 408 ms, Memory: 132136 KB -BaekjoonHub

--- 백준/Gold/10800. 컬러볼/컬러볼.py
+++ 백준/Gold/10800. 컬러볼/컬러볼.py
@@ -2,37 +2,26 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-colors = [0]*n
-result = [0]*n
-balls = []
+size = [[] for _ in range(2001)]
+color = [0]*n
+Sum = 0
+
 for i in range(n):
-    balls.append(list(map(int, input().split()))+[i])
+    c, s = map(int, input().split())
+    size[s].append((c-1, i))
+    Sum += s
+    color[c-1] += s
 
-size_balls = sorted(balls, key=lambda x: (x[1], x[0]))
 
-now, total = 0, 0
-result_temp = 0
-minus = 0
-prev_color, prev_weight = size_balls[0][0], size_balls[0][1]
-for i in range(n):
-    [color, weight, index] = size_balls[i]
+answer = [0]*n
 
-    if prev_color == color and weight == prev_weight:
-        result[index] = result_temp
-    else:
-        if weight == prev_weight:
-            minus = colors[color-1]
-        else:
-            now = total
-            minus = colors[color-1]
+for siz in range(2000, 0, -1):
+    for a, ind in size[siz]:
+        Sum -= siz
+        color[a] -= siz
 
-        result[index] = now - minus
-        result_temp = result[index]
+    for a, ind in size[siz]:
+        answer[ind] = Sum - color[a]
 
-    total += weight
-    colors[color-1] += weight
-
-    prev_color, prev_weight = color, weight
-
-for res in result:
-    print(res)
+for ans in answer:
+    print(ans)
