aa5db6c6f95dae4f53c4c0df996dd60d4067aeb0
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-05-19 10:39:11 +0900

    [Bronze II] Title: 대표값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2592. 대표값/대표값.cc
+++ 백준/Bronze/2592. 대표값/대표값.cc
@@ -4,30 +4,25 @@
 #include <numeric>
 
 using namespace std;
+int arr[100], total, ind, r;
 int main()
 {
-    int arr[1000] = {0};
-    int total = 0;
-    int maxi = 0;
-    int index, r;
 
     for (int i = 0; i < 10; i++)
     {
         cin >> r;
-        arr[r] += 1;
+        arr[r / 10]++;
         total += r;
     }
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < 100; i++)
     {
-        if (maxi < arr[i])
-        {
-            maxi = arr[i];
-            index = i;
-        }
+        if (arr[ind] < arr[i])
+            ind = i;
     }
 
     cout << total / 10 << "\n"
-         << index;
+         << ind * 10;
+
     return 0;
 }
