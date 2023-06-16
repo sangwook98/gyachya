ab0194c9f46f5bf7ff6a827c9a11cc48b7321c05
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-06-16 11:25:45 +0900

    [Gold III] Title: 텀 프로젝트, Time: 492 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,42 +1,45 @@
-#include <cstdio>
-#include <queue>
-#include <string>
-#include <vector>
+#include<iostream>
+#include<cstring>
+#include<queue>
+
 using namespace std;
 
-int t, n;
-int child[100001];
-int count[100001];
+int student[100001];
+int indegree[100001];
 
-int main() {
-  scanf("%d", &t);
-  while (t--) {
-    scanf("%d", &n);
-    int answer = 0;
-    fill_n(count, n + 1, 0);
-    queue<int> q;
-    for (int i = 1; i < n + 1; ++i) {
-      scanf("%d", &child[i]);
-      ++count[child[i]];
-    }
-    for (int i = 1; i < n + 1; ++i) {
-      if (count[i] == 0) {
-        q.push(i);
-        ++answer;
-      }
-    }
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-    while (!q.empty()) {
-      int a = q.front();
-      q.pop();
-      --count[child[a]];
-      if (count[child[a]] == 0) {
-        q.push(child[a]);
-        ++answer;
-      }
+    int T; cin >> T;
+    for (int test_case = 1; test_case <= T; test_case++)
+    {   // 입력
+        int N; cin >> N;
+        for (int i = 1; i <= N; i++)
+        {
+            cin >> student[i];
+            indegree[student[i]]++;
+        }
+        // 위상정렬 -> 사이클을 이루는 원소는 항상 indegree > 0이므로 큐에 들어갈 수 없음.
+        int ans = 0;
+        queue<int> Q;
+        for (int i = 1; i <= N; i++)
+        {
+            if (indegree[i] == 0)
+                Q.push(i);
+        }
+        while (!Q.empty())
+        {
+            int cur = Q.front(); Q.pop();
+            if (--indegree[student[cur]] == 0)
+                Q.push(student[cur]);
+            ans++;
+        }
+        cout << ans << '\n';
+        // TC 초기화
+        memset(student, 0, sizeof(student));
+        memset(indegree, 0, sizeof(indegree));
     }
-    printf("%d", answer);
-    if (t)
-      printf("\n");
-  }
+    return 0;
 }
