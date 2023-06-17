e67503ceb288a4ab00eb3c81edd819624256d40b
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-06-17 11:23:49 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 420 ms, Memory: 18200 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -3,14 +3,14 @@
 #include <string>
 using namespace std;
 struct Node {
-  int x, y, cnt;
-  Node(int a, int b, int c) { x = a, y = b, cnt = c; }
+  int x, y, dis, wall;
+  Node(int a, int b, int c, int d) { x = a, y = b, dis = c, wall = d; }
 };
 int INF{1000001};
 
-int n, m, k;
+int n, m, k, answer{INF};
 int graph[1001][1001];
-int dis[1001][1001][11];
+bool visited[1001][1001][11];
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
@@ -25,40 +25,37 @@ int main() {
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
       scanf("%1d", &graph[i][j]);
-      fill_n(dis[i][j], k + 1, INF);
     }
   }
-  fill_n(dis[0][0], k + 1, 1);
 
   queue<Node> q;
-  q.push({0, 0, k});
+  q.push({0, 0, 1, k});
+  visited[0][0][k] = true;
 
   while (!q.empty()) {
-    int x = q.front().x, y = q.front().y, cnt = q.front().cnt;
+    int x = q.front().x, y = q.front().y, dis = q.front().dis,
+        wall = q.front().wall;
     q.pop();
-    if (x == n - 1 && y == m - 1)
+    if (x == n - 1 && y == m - 1) {
+      answer = dis;
       break;
+    }
 
     for (int k = 0; k < 4; ++k) {
       int nx = x + dx[k], ny = y + dy[k];
-      if (check(nx, ny)) {
-        if (graph[nx][ny] == 0 && dis[nx][ny][cnt] > dis[x][y][cnt] + 1) {
-          dis[nx][ny][cnt] = dis[x][y][cnt] + 1;
-          q.push({nx, ny, cnt});
-        } else if (graph[nx][ny] == 1 && cnt > 0 &&
-                   dis[nx][ny][cnt - 1] > dis[x][y][cnt] + 1) {
-          dis[nx][ny][cnt - 1] = dis[x][y][cnt] + 1;
-          q.push({nx, ny, cnt - 1});
+      if (check(nx, ny) && !visited[nx][ny][wall]) {
+        if (graph[nx][ny] == 0) {
+          visited[nx][ny][wall] = true;
+          q.push({nx, ny, dis + 1, wall});
+        } else if (graph[nx][ny] == 1 && wall > 0 &&
+                   !visited[nx][ny][wall - 1]) {
+          visited[nx][ny][wall - 1] = true;
+          q.push({nx, ny, dis + 1, wall - 1});
         }
       }
     }
   }
 
-  int answer = INF;
-  for (int i = 0; i <= k; ++i) {
-    if (answer > dis[n - 1][m - 1][i])
-      answer = dis[n - 1][m - 1][i];
-  }
   answer = answer == INF ? -1 : answer;
   printf("%d", answer);
 
