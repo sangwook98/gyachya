5a6ada214d9e8c0d6fe76c6be15268a2678eceed
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-07-21 12:21:59 +0900

    [level 2] Title: 소수 찾기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.cpp
@@ -17,7 +17,7 @@ bool is_prime(int x){
     if(x == 0 || x == 1)
         return false;
     
-    for(int i =2;i<sqrt(x)+1;i++){
+    for(int i =2;i<=sqrt(x);i++){
         if(x%i == 0)
             return false;
     }
@@ -30,7 +30,7 @@ void dfs(string num){
     
     if(num.size() > 0){
         int a = stoi(num);
-        if(!not_prime[a] &&!num_visited[a]){
+        if(is_prime(a) &&!num_visited[a]){
             num_visited[a]=true;
             // printf("%s\n",num.c_str());
             answer++;
@@ -58,13 +58,13 @@ int solution(string numbers_) {
     n = numbers.size();
     int maxi = pow(10,n);
     
-    not_prime[0] = true, not_prime[1]=true;
-    for(int i = 2;i<maxi;i++){
-        if(!not_prime[i]){
-            for(int j =i*2;j<maxi;j+=i)
-                not_prime[j]=true;
-        }
-    }
+    // not_prime[0] = true, not_prime[1]=true;
+    // for(int i = 2;i<maxi;i++){
+    //     if(!not_prime[i]){
+    //         for(int j =i*2;j<maxi;j+=i)
+    //             not_prime[j]=true;
+    //     }
+    // }
     
     dfs("");
     
