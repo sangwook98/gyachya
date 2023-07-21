48ff255e70189c97138f28b3ff5b251a33a0389c
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-07-21 12:20:17 +0900

    [level 2] Title: 소수 찾기, Time: 0.36 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,72 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+bool not_prime[10000000];
+string numbers;
+int answer;
+int n;
+
+bool visited[8];
+bool num_visited[10000000];
+
+bool is_prime(int x){
+    if(x == 0 || x == 1)
+        return false;
+    
+    for(int i =2;i<sqrt(x)+1;i++){
+        if(x%i == 0)
+            return false;
+    }
+    
+    return true;
+}
+
+void dfs(string num){
+
+    
+    if(num.size() > 0){
+        int a = stoi(num);
+        if(!not_prime[a] &&!num_visited[a]){
+            num_visited[a]=true;
+            // printf("%s\n",num.c_str());
+            answer++;
+        }
+
+    }
+       
+    if(num.size() == n)
+        return;
+
+    for(int i =0;i<n;i++){
+        if(!visited[i]){
+            visited[i]=true;
+            dfs(num+numbers[i]);
+            visited[i]=false;
+        }
+
+    }
+        
+    
+}
+
+int solution(string numbers_) {
+    numbers = numbers_;
+    n = numbers.size();
+    int maxi = pow(10,n);
+    
+    not_prime[0] = true, not_prime[1]=true;
+    for(int i = 2;i<maxi;i++){
+        if(!not_prime[i]){
+            for(int j =i*2;j<maxi;j+=i)
+                not_prime[j]=true;
+        }
+    }
+    
+    dfs("");
+    
+    return answer;
+}
