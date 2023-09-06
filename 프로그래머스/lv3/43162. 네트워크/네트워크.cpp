d366a2ecc4db686aeeac3d0e855b2077a8d88cb0
sangwook98 <93751703+sangwook98@users.noreply.github.com>
2023-09-06 09:23:18 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -1,32 +1,34 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 bool visited[201];
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    for (int i = 0;i<n;i++){
+    
+    for(int i =0;i<n;i++){
         if(!visited[i]){
             answer++;
             visited[i]= true;
-            queue<int> q({i});
-            
+            queue<int> q;
+            q.push(i);
             while(!q.empty()){
                 int x = q.front();
                 q.pop();
-                
-                for(int j = 0;j<n;j++){
-                    if(computers[x][j] && !visited[j]){
-                        visited[j]= true;
+                for(int j=0;j<n;j++){
+                    if(!visited[j] && computers[x][j]){
+                        visited[j]=true;
                         q.push(j);
                     }
-                }    
+                }
             }
-            
-            
         }
     }
+    
+    
+    
     return answer;
 }
